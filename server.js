c990a6dbd92c5a9b6b769168054eb84aa1464196
Franck <franck.abgrall@zenika.com>
2018-10-03 13:31:12 +0200

    https auto redirect

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 
 const express = require('express');
+const path = require('path');
 const app = express();
 
-app.get('*', (req, res) => {  
-  res.redirect('https://' + req.headers.host + req.url);
+app.get('*', (req, res, next) => {  
+  if (!req.sercure) 
+    return res.redirect('https://' + req.headers.host + req.url);
+  else
+    next()
 })
+
+app.get('/*', (req, res) =>
+ res.sendFile(path.join(__dirname, './dist'))
+);
 
 app.listen(process.env.PORT);
